0232bc65845a49c68e7315a182cc3442e8bd65ba
Daniel Box <danieljbox-work@outlook.com>
2025-03-07 06:15:39 +1000

    Add robofriends updates

--- react-fundamentals/robofriends/src/main.jsx
+++ react-fundamentals/robofriends/src/main.jsx
@@ -1,11 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import React from 'react';
+// import ReactDOM from 'react-dom'; The new way to import createRoot:
+import { createRoot } from "react-dom/client";
+import './index.css'; 
+import App from './containers/App';
 import 'tachyons';
+// import registerServiceWorker from './registerServiceWorker';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
+
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
